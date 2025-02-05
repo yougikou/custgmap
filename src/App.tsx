2bf980894b0874eae5fe7d2e7c453aab679d947f
yougikou <yougikou@gmail.com>
2025-02-06 00:38:16 +0900

    add page support and markdown wiki

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,40 @@
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import './App.css';
 import SidePanel from './components/SidePanel.tsx';
+import Wiki from './components/Wiki.tsx';
 
 function App() {
+  const basename = process.env.NODE_ENV === 'production' ? '/custgmap' : '';
 
   return (
-    <div className="App">
-      <SidePanel />
-      <div style={{ height: '100vh', width: '100vw', position: 'fixed', top: 0, left: 0 }}>
-          <iframe 
-            title="Google Map"
-            style={{ width: '100%', height: '100%', border: 'none' }}
-            id="gmap_canvas" 
-            src="https://maps.google.com/maps?q=Tokyo&t=&z=13&ie=UTF8&iwloc=&output=embed" 
-            frameBorder="0" 
-            scrolling="no" 
-            marginHeight="0" 
-            marginWidth="0"/>
+    <Router basename={basename}>
+      <div className="App">
+        <nav className="app-nav">
+          <Link to="/">Map</Link>
+          <Link to="/wiki">Wiki</Link>
+        </nav>
+        <Routes>
+          <Route path="/wiki" element={<Wiki />} />
+          <Route path="/" element={
+            <>
+              <SidePanel />
+              <div style={{ height: '100vh', width: '100vw', position: 'fixed', top: 0, left: 0 }}>
+                <iframe 
+                  title="Google Map"
+                  style={{ width: '100%', height: '100%', border: 'none' }}
+                  id="gmap_canvas" 
+                  src="https://maps.google.com/maps?q=Tokyo&t=&z=13&ie=UTF8&iwloc=&output=embed" 
+                  frameBorder="0" 
+                  scrolling="no" 
+                  marginHeight={0} 
+                  marginWidth={0}/>
+              </div>
+            </>
+          } />
+        </Routes>
       </div>
-    </div >
+    </Router>
   );
 }
 
